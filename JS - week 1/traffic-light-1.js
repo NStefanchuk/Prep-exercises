c4bd912d30d30dfe252d0adbf7d294af56faba61
Nikita Stefanchuk <nik.stefanchuk@gmail.com>
2025-02-14 12:56:24 +0100

    My prep exercises

---
+++ JS - week 1/traffic-light-1.js
@@ -0,0 +1,23 @@
+'use strict'
+const trafficLight = {
+  state: 'green',
+}
+let rotations = 0
+console.log('The traffic light is on', trafficLight.state)
+
+while (rotations < 2) {
+  const currentState = trafficLight.state
+  if (trafficLight.state === 'green') {
+    trafficLight.state = 'orange'
+  } else if (trafficLight.state === 'orange') {
+    trafficLight.state = 'red'
+  } else if (trafficLight.state === 'red') {
+    trafficLight.state = 'green'
+    rotations++
+
+    if (rotations === 2) {
+      break
+    }
+  }
+  console.log('The traffic light is on', trafficLight.state)
+}
